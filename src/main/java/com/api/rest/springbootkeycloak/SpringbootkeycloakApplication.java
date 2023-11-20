2abdb999fe83a3b9735f5588c8d184394e027b31
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2023-11-20 20:52:52 +0100

    first commit

---
+++ src/main/java/com/api/rest/springbootkeycloak/SpringbootkeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.api.rest.springbootkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootkeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootkeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/rest/springbootkeycloak/SpringbootkeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.rest.springbootkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootkeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
