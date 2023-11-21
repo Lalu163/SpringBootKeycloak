5ca0046496f5b6115ce5277153d8a1484613156f
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2023-11-21 22:26:43 +0100

    añadidos roles en testController

--- src/main/java/com/api/rest/springbootkeycloak/controller/TestControler.java
+++ src/main/java/com/api/rest/springbootkeycloak/controller/TestControler.java
@@ -1,5 +1,6 @@
 package com.api.rest.springbootkeycloak.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,11 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestControler {
     
     @GetMapping("/hello-1")
+    @PreAuthorize("hasRole('admin_client-role')")
     public String helloAdmin(){
         return "Hello Spring Boot With Keycloak - ADMIN";
     }
 
     @GetMapping("/hello-2")
+    @PreAuthorize("hasRole('user_client-role') or hasRole('admin_client_role')")
     public String helloUser(){
         return "Hello Spring Boot With Keycloak - USER";
     }
